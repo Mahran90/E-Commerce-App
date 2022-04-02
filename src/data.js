6babbf987ff002d636f4c74662f6b0c453232ee2
Mahran90 <m.sarko1990@gmail.com>
2022-04-02 21:35:06 +0200

    Initial Commit
    First commit to the Repo (React E-Commerce-App) includes the basics

---
+++ src/data.js
@@ -0,0 +1,91 @@
+import summer from './imgs/summer.png'
+import autumn from './imgs/autumn.png'
+import popular from './imgs/popular.png'
+import shirts from './imgs/shirts.jpg'
+import loungewear from './imgs/loungewear.jpg'
+import lightjackets from './imgs/lightjackets.jpg'
+import product_1 from './imgs/product_1.png'
+import product_2 from './imgs/product_2.png'
+import product_3 from './imgs/product_3.png'
+import product_4 from './imgs/product_4.png'
+import product_5 from './imgs/product_5.png'
+import product_6 from './imgs/product_6.png'
+import product_7 from './imgs/product_7.png'
+import product_8 from './imgs/product_8.png'
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: summer,
+        title: 'SUMMER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'f5fafd'
+    },
+    {
+        id: 2,
+        img: autumn,
+        title: 'AUTUMN COLLECTION',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'fcf1ed'
+    },
+    {
+        id: 3,
+        img: popular,
+        title: 'POPULAR COLLECTION',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'fbf0f4'
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: shirts,
+        title: 'T-SHIRT STYLE!'
+    },
+    {
+        id: 2,
+        img: loungewear,
+        title: 'LOUNGEWEAR LOVE'
+    },
+    {
+        id: 3,
+        img: lightjackets,
+        title: 'LIGHT JACKETS'
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: product_1,
+    },
+    {
+        id: 2,
+        img: product_2,
+    },
+    {
+        id: 3,
+        img: product_3,
+    },
+    {
+        id: 4,
+        img: product_4,
+    },
+    {
+        id: 5,
+        img: product_5,
+    },
+    {
+        id: 6,
+        img: product_6,
+    },
+    {
+        id: 7,
+        img: product_7,
+    },
+    {
+        id: 8,
+        img: product_8,
+    },
+]
